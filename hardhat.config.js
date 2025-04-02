52301f9258a8902f4bdc7e36036adf63b1caa855
Robert McElhinney <robmcelhinney@hotmail.com>
2025-04-02 19:11:58 +0100

    deploy to base

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,11 @@ require("dotenv").config()
 module.exports = {
     solidity: "0.8.28",
     networks: {
-        // "base-mainnet": {
-        //     url: "https://mainnet.base.org",
-        //     accounts: [process.env.WALLET_PRIVATE_KEY],
-        //     gasPrice: 1000000000,
-        // },
+        "base-mainnet": {
+            url: "https://mainnet.base.org",
+            accounts: [process.env.WALLET_PRIVATE_KEY],
+            gasPrice: 1000000000,
+        },
         "base-sepolia": {
             url: "https://sepolia.base.org",
             accounts: [process.env.WALLET_PRIVATE_KEY],
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ async function main() {
     console.log("Deploying contracts with account:", deployer.address)
 
     const ZKBadgeNFT = await ethers.getContractFactory("ZKBadgeNFT")
-    const verifierAddress = "0x63E1cE0eFd672fe487A02eB373e155FB3C9225d3" // update with your deployed verifier address
+    const verifierAddress = "0xB945121b28F2Fb2043e3290C3147874ae4d31289" // update with your deployed verifier address
 
     const zkBadgeNFT = await ZKBadgeNFT.deploy(verifierAddress)
     await zkBadgeNFT.waitForDeployment()
